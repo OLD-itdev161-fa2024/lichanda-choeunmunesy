efd20b39bebbbd631b85dfba9dddadb7e0debf14
Lanna <lanna@lanna.com>
2023-10-03 13:15:21 -0500

    Update

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ import express from 'express';
 import connectDatabase from './config/db';
 
 
+
+
+
+
 const app = express();
 
 
+
+
 connectDatabase();
+
+
+app.use(express.json({extended: false}));
+
+
+/**
+*@route
+*@desc
+*/
+
+
+
+
 
 
 app.get('/', (req, res) =>
@@ -13,4 +32,16 @@ app.get('/', (req, res) =>
 );
 
 
+/**
+ * @route
+ * @desc
+ */
+app.post('/api/users', (req, res) => {
+console.log(req.body);
+res.send(req.body);
+});
+
+
 app.listen(3000, () => console.log('Express server running on port 3000'));
+
+
