75dd02c8c61f9d421f057f3ba3d8129d02a1434a
Lanna <lanna@lanna.com>
2023-10-05 13:50:02 -0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component{
+  state = {
+    data: null
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000')
+    .then((response) => {
+      this.setState({
+        data: response.data
+      })
+    })
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    })
+  }
+
+  render() {
+    return (
+      <div className = 'App'>
+      <header className='App-header'>
+        GoodThings
       </header>
-    </div>
-  );
+      {this.state.data}
+      </div>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import connectDatabase  from './config/db';
 import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 
+//Initialize express application
 const app = express();
 
+//Connect database
 connectDatabase();
 
+//Configure Middleware
 app.use(express.json({ extended: false}));
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+)
 
 //API endpoints
 /**
@@ -42,4 +51,5 @@ app.post('/api/users',
         }
 );
 
-app.listen(3000, () => console.log('Express server running on port 3000'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
